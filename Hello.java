7c1e92432e16543fa3da71d70cf96784c5a9dc7b
youngwoo011 <chu8354@gmail.com>
2026-03-25 18:16:30 +0900

    주석 내용 추가

--- Hello.java
+++ Hello.java
@@ -5,10 +5,11 @@ public class Hello {
 	public void print(String name) {
 		System.out.println("Hello! " + name);
 		}
-
+	
+	//진입 메서드
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Hello hello = new Hello();
+		Hello hello = new Hello(); //new 키워드를 사용해 객체 생성
 		Hello hi = new Hello();
 		hello.print("Java");
 		hi.print("hi");
